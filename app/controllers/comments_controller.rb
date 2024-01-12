4e6edc0234965999d21157e01de79bd7109798d0
SAORI KODERA <tibezu.dash@gmail.com>
2024-01-12 15:11:01 +0900

    書き換えのため一時保管

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  before_action :set_blog
+
+  def create
+    @comment = @blog.comments.build(comment_params)
+    respond_to do |format|
+
+      format.html
+      format.js
+
+      if @comment.save
+        flash.now[:notice] = 'コメントが投稿されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの投稿に失敗しました'
+        format.js { render :index }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description, :blog_id)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
+end
